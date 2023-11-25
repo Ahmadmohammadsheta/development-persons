ad8afe304a57d43c83c70f81237df4b7a0a53de4
Ahmad Mohammad Sheta <Ahmadmohammadsheta@gmail.com>
2023-11-26 00:09:09 +0200

    CORS_ALLOWED_ORIGINS frontend in .env

--- config/cors.php
+++ config/cors.php
@@ -19,11 +19,15 @@ return [
 
     'allowed_methods' => ['*'],
 
-    // 'allowed_origins' => ['*'],
-    'allowed_origins' => ['http://localhost:8080'],
     // ama.config allowed_origins
+    // 'allowed_origins' => ['*'],
+    // 'allowed_origins' => [env('Frontend URL', 'default')],
+    'allowed_origins' => [env('CORS_ALLOWED_ORIGINS', 'http://localhost:8000')],
+    // 'allowed_origins' => ['http://localhost:8080'],
 
     'allowed_origins_patterns' => [],
+    // use this configure & make allowed_origins null
+    // 'allowed_origins_patterns' => ["*localhost*"],
 
     'allowed_headers' => ['*'],
 
@@ -36,3 +40,70 @@ return [
     'supports_credentials' => true,
 
 ];
+
+// ama.config example
+// return [
+
+//     /*
+//      * The allowed origins that can make cross-origin requests.
+//      *
+//      * @var array
+//      */
+//     'allowed_origins' => [
+//         env('http://localhost:8080', 'http://localhost:8000'),
+//     ],
+
+//     /*
+//      * Allowed methods for CORS requests.
+//      *
+//      * @var array
+//      */
+//     'allowed_methods' => [
+//         'GET',
+//         'POST',
+//         'PUT',
+//         'PATCH',
+//         'DELETE',
+//         'OPTIONS',
+//     ],
+
+//     /*
+//      * Allowed headers for CORS requests.
+//      *
+//      * @var array
+//      */
+//     'allowed_headers' => [
+//         '*',
+//     ],
+
+//     /*
+//      * Exposed headers for CORS requests.
+//      *
+//      * @var array
+//      */
+//     'exposed_headers' => [
+//         'Authorization',
+//         'Content-Type',
+//     ],
+
+//     /*
+//      * Max age for CORS requests.
+//      *
+//      * @var int
+//      */
+//     'max_age' => 60 * 60,
+
+//     /*
+//      * Whether to allow credentials for CORS requests.
+//      *
+//      * @var bool
+//      */
+//     'allow_credentials' => true,
+
+//     /*
+//      * Whether to provide preflight responses for CORS requests.
+//      *
+//      * @var bool
+//      */
+//     'allow_preflight' => true,
+// ];
